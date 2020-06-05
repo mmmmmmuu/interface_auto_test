1108c08a78d72a0f06bf50cdc839f6608b6de927
springconfig <lizhiyang@happycfc.com>
2020-06-05 18:03:12 +0800

    init

---
+++ src/p0constants/base_constants.py
@@ -0,0 +1,4 @@
+BASE_TABLES = ["batch_info", "synd_mapping", "table_config"]
+INTERVAL = 1
+TIMES = 120
+THREADNUM = 1
---
+++ src/p0constants/batch_constants.py
@@ -0,0 +1,5 @@
+
+BATCH_BATCH = "batch"
+BATCH_GROUP = "group"
+BATCH_INTERFACE = "interface"
+
