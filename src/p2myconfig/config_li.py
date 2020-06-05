8757c143a1d646fac47cc58334de5d0130ac7a06
springconfig <lizhiyang@happycfc.com>
2020-06-05 18:23:47 +0800

    init

---
+++ src/p2myconfig/config_li.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+# url
+import datetime
+
+product_db = {
+    "host": "10.1.81.7"
+    , "port": 49159
+    , "db": "calculator_xfm_lhd"
+    , "user": "root"
+    , "passwd": "root"
+}
+
+# test_db
+test_db = {"host": "10.1.81.62"
+    , "port": 3306
+    , "db": "test_calculator"
+    , "user": "root"
+    , "passwd": "root"
+           }
